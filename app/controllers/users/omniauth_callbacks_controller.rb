09b8cbb3498be6612d3db82633d3fc27c09aee93
victorhazbunanuff <victor.hazbun@koombea.com>
2012-10-13 10:03:41 -0500

    redirections for steps

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_filter :user_incompleted
 
   def method_missing(provider)
     auth = request.env["omniauth.auth"]
     @user = User.from_omniauth(auth)
-    sign_in @user
-    flash[:notice] = "User signed in sucessfully"
-    redirect_to root_path
+    if auth["provider"] != "foursquare"
+      redirect_to user_step_path(:networks)
+    else
+      sign_in @user
+      redirect_to root_path
+    end
   end
 end
