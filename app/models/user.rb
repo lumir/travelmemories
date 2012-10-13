a17c02764eebd59093d219263e355e10418a16e9
victorhazbunanuff <victor.hazbun@koombea.com>
2012-10-12 22:12:44 -0500

    login with foursquare

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,31 @@
 class User < ActiveRecord::Base
-  include OmniauthAuthenticationHelper
   # Devise options - Start
     devise :database_authenticatable, :recoverable, :trackable, :omniauthable
   # Devise options - Ends
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+
+  has_many :authentications, dependent: :destroy
+
+  def self.apply_omniauth(auth)
+    authentication = Authentication.find_by_uid(auth["uid"])
+    if authentication.blank?
+      user = User.new(first_name: auth["info"]["first_name"], last_name: auth["info"]["last_name"], email: auth["info"]["email"])
+      user.save
+      authentication = Authentication.new(provider: auth["provider"], uid: auth["uid"], token: auth["credentials"]["token"], secret: auth["credentials"]["secret"], user_id: user.id)
+        authentication.save
+    else
+      user = authentication.user
+    end
+    user
+  end
 
   protected
 
   def password_required?
     (authentications.empty? || password.present?) && super
   end
+
+
 end
--- extras/oauth_foursquare.rb
+++
@@ -1,7 +0,0 @@
-class OauthFoursquare < OauthGeneric
-
-  def self.network_url(access_token)
-    "https://foursquare.com/user/"+access_token["uid"]
-  end
-
-end
