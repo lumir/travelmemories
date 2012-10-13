6ea65137f56d33a967191611e02e562b2b4ba48c
lumir <lumir.olivares@koombea.com>
2012-10-12 19:23:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelmemories::Application.config.secret_token = '68cd16be05da38dfe9303ff5e9264aaa55085a8d3fab689da991c7e7cfb91b380ec8f77731e622bf801cc6f48d1c5fa57bf2901a7e0c85f25d3d5325973a02f6'
