5c246a20bf958100494f0c4e6c2dd6bea093e2f9
lumir <lumir.olivares@koombea.com>
2012-10-12 23:57:15 -0500

    removing coffe files

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,8 @@
 class Friendship < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :user
+  belongs_to :friend, class_name: "User", :foreign_key => "friend_id"
+
+  validates_uniqueness_of :friend_id, :scope => :user_id
+
+  attr_accessible :user_id, :friend_id, :accepted,
 end
