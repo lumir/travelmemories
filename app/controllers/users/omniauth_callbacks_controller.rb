540ec25890e9e42f8d5c84756cfcc176fa0477c2
lumir <lumir.olivares@koombea.com>
2012-10-13 09:50:30 -0500

    list facebook friends

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def method_missing(provider)
     auth = request.env["omniauth.auth"]
-    @user = User.from_omniauth(auth)
-    sign_in @user
+    @user = User.from_omniauth(auth, current_user)
+    sign_in @user unless auth["provider"] == "facebook"
     flash[:notice] = "User signed in sucessfully"
     redirect_to root_path
   end
