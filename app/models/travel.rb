a29e05494a493b1bab8e9d343d9b9f2596fe6eac
victorhazbunanuff <victor.hazbun@koombea.com>
2012-10-14 11:04:50 -0500

    add attr_accessible name

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,6 +1,6 @@
 class Travel < ActiveRecord::Base
 
-  attr_accessible :user_id, :location, :start_date, :end_date
+  attr_accessible :user_id, :location, :start_date, :end_date, :name
 
   has_many :checkins
   has_many :photos
