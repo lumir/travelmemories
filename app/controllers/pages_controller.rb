612b1b8716f82eac2b67ea4ab7bd1b5bbd46df2e
Fabian Sabogal <fabian.sabogal@koombea.com>
2012-10-14 11:20:11 -0500

    adding markup for photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!, only: [:index]
 
   def index
+    if user_signed_in?
+      redirect_to current_user.incompleted? ? user_step_path("networks") : travels_path
+    end
   end
 
 end
